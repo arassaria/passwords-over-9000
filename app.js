b6afa91c429393dd0de87d70f9e8d97acfd1a1db
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-10 12:32:51 +0100

    Install inquirer

--- app.js
+++ app.js
@@ -2,28 +2,55 @@ const inquirer = require("inquirer");
 
 const password = "test";
 
-let questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What's your name?",
-  },
+const args = process.argv.slice(2);
+
+const questions = [
   {
     type: "password",
     name: "password",
-    message: "Enter your password:",
+    message: "Enter the master-password:",
   },
-  {
-    type: "input",
-    name: "feeling",
-    message: "How do you feel today?",
-  },
+  //   {
+  //     type: "checkbox",
+  //     name: "feeling",
+  //     message: "How do you feel today?",
+  //     choices: ["Awesome!", "Good", "Sad", "I feel like hacking :D"],
+  //   },
+  //   {
+  //     type: "checkbox",
+  //     name: "Status",
+  //     choices: ["Owner", "Hacker"],
+  //   },
 ];
 
 inquirer.prompt(questions).then((answers) => {
-  console.log(`Hi ${answers["name"]}!`);
-  console.log(`Glad to hear you feel ${answers["feeling"]}.`);
-  console.log(
-    `But you entered the wrong password, so go away, you filthy hacker.`
-  );
+  //   switch (answers["feeling"]) {
+  //     case ["Awesome!"]:
+  //       console.log(
+  //         `I love to hear that you are feeling ${answers["feeling"]} today.`
+  //       );
+  //       break;
+  //     case ["Good"]:
+  //       console.log(`Glad you are feeling ${answers["feeling"]} today.`);
+  //       break;
+  //     case ["Sad"]:
+  //       console.log(
+  //         `If you enter "my Penis" as password, it will respond with "Too short". Hope this little Easter Egg can cheer up your mood. ;)`
+  //       );
+  //       break;
+  //     case ["I feel like hacking :D"]:
+  //       console.log(`You should get a life, boy.`);
+  //       break;
+  //     default:
+  //       console.log(`You have not chosen a mood for today.`);
+  //       break;
+  //   }
+  //   console.log(`You are my ${answers["Status"]}`);
+  if (args[0] === "Dominique" && answers["password"] === password) {
+    console.log(`Hi ${args[0]}!`);
+  } else if (answers["password"] === "my Penis") {
+    console.log(`Too Short!`);
+  } else {
+    console.log(`Go away, you filthy Hacker, ${args[0]}`);
+  }
 });
