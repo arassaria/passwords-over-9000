90dede5ed75ef6c6508fb1733cfd514546c9e023
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-10 17:35:01 +0100

    Add different questions

--- app.js
+++ app.js
@@ -1,56 +1,57 @@
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
-const password = "test";
-
-const args = process.argv.slice(2);
+const secretMasterPassword = "test";
 
 const questions = [
   {
     type: "password",
-    name: "password",
+    name: "masterPassword",
     message: "Enter the master-password:",
   },
-  //   {
-  //     type: "checkbox",
-  //     name: "feeling",
-  //     message: "How do you feel today?",
-  //     choices: ["Awesome!", "Good", "Sad", "I feel like hacking :D"],
-  //   },
-  //   {
-  //     type: "checkbox",
-  //     name: "Status",
-  //     choices: ["Owner", "Hacker"],
-  //   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  //   switch (answers["feeling"]) {
-  //     case ["Awesome!"]:
-  //       console.log(
-  //         `I love to hear that you are feeling ${answers["feeling"]} today.`
-  //       );
-  //       break;
-  //     case ["Good"]:
-  //       console.log(`Glad you are feeling ${answers["feeling"]} today.`);
-  //       break;
-  //     case ["Sad"]:
-  //       console.log(
-  //         `If you enter "my Penis" as password, it will respond with "Too short". Hope this little Easter Egg can cheer up your mood. ;)`
-  //       );
-  //       break;
-  //     case ["I feel like hacking :D"]:
-  //       console.log(`You should get a life, boy.`);
-  //       break;
-  //     default:
-  //       console.log(`You have not chosen a mood for today.`);
-  //       break;
-  //   }
-  //   console.log(`You are my ${answers["Status"]}`);
-  if (args[0] === "Dominique" && answers["password"] === password) {
-    console.log(`Hi ${args[0]}!`);
-  } else if (answers["password"] === "my Penis") {
-    console.log(`Too Short!`);
-  } else {
-    console.log(`Go away, you filthy Hacker, ${args[0]}`);
+async function getData() {
+  const promise = await fs.readFile("./db.json", "utf8");
+  const data = await JSON.parse(promise);
+  return data;
+}
+
+async function validateAccess() {
+  let exit = 0;
+  const { masterPassword } = await inquirer.prompt(questions);
+  if (secretMasterPassword !== masterPassword) {
+    console.error(`Go away, you filthy Hacker!`);
+    return;
   }
-});
+  while (exit === 0) {
+    console.log(`Fuck, how did you get the super secret master password?`);
+    const { passwordName } = await inquirer.prompt({
+      type: "input",
+      name: "passwordName",
+      message: "What password do you want to know?",
+    });
+
+    const passwordSafe = await getData();
+
+    const password = passwordSafe[passwordName];
+    if (password) {
+      console.log(`${passwordName}: ${password}`);
+    } else {
+      console.log(
+        "Very smart, Hacker. Asking for a password that does not exist."
+      );
+    }
+    const { more } = await inquirer.prompt({
+      type: "list",
+      name: "more",
+      message: "Do you want to look for more passwords?",
+      choices: ["Yes", "No"],
+    });
+    if (!more.includes("Yes")) {
+      exit = 1;
+    }
+  }
+}
+
+validateAccess();
